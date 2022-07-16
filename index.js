2415b9fd134d4aed99cba110ef651c35a5e15971
leandrozulis <leandro_zulli@hotmail.com>
2022-07-16 17:23:52 -0300

    Projeto Finalizado

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-import tempo from './data/dados.js';
+import colecaoChuvasDeMeteoros from "./data/chuva-de-meteoros.js";
+import imprimeListaDeChuvas from "./funcoes/funcoesInterface.js";
+import {
+    verificaChuvaVisivelPorData,
+    verificaChuvaVisivelProximos2Meses
+} from "./funcoes/funcoesLogicas.js";
 
-let dataAtual = new Date();
+const dataAtual = new Date();
 
-let dia = `${dataAtual.getMonth()}/${dataAtual.getDate()}`;
-console.log(dia);
+const chuvasVisiveisHoje = colecaoChuvasDeMeteoros.filter(
+    (chuva)=> verificaChuvaVisivelPorData(chuva, dataAtual)
+);
 
-const filtrarDataHoje = (arr) => {
-  arr.forEach( data => data);
+const chuvasVisiveisProximos2Meses = colecaoChuvasDeMeteoros.filter(
+    (chuva)=> verificaChuvaVisivelProximos2Meses(chuva, dataAtual)
+);
+
+console.log('Chuva de meteoros');
+
+if ( chuvasVisiveisHoje.length > 0 ) {
+    let msg = "\nEncontramos ";
+    msg += chuvasVisiveisHoje.length == 1
+        ? '1 chuva de meteoros que pode ser vista hoje'
+        : chuvasVisiveisHoje.length + ' chuvas de meteoros que podem ser vistas hoje';
+
+    console.log(msg);
+
+    imprimeListaDeChuvas(chuvasVisiveisHoje);
+} else {
+    console.log('\nNenhuma chuva de meteoros passando no momento');
 }
 
-console.log(filtrarDataHoje(tempo));
-
-// return arr.inicioDaChuva;
-// console.log(dataAtual);
+console.log('\n\nNão perca as próximas chuvas de meteoros:');
+imprimeListaDeChuvas(chuvasVisiveisProximos2Meses);
